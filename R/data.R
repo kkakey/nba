e26d7fa616c88a9b7fef567e02944f09afee701f
kka2120 <kkakey999@gmail.com>
2020-03-03 11:51:17 -0500

    Update data.R file contents and rename DATASET.R to nba_df.R

--- R/data.R
+++ R/data.R
@@ -1,7 +1,9 @@
 #' NBA Statistics
-#'
+#' @docType data
 #' @source NBA Seasons Stats from \href{https://www.kaggle.com/drgilermo/nba-players-stats/data}{kaggle}
 #' @format A data frame with 52 columns.
+#' @keywords datasets
+#' @usage data(nba_df)
 
 #' @examples
 #' \dontrun{
